7e8efd99edf953a0e4ae8b3a737205a8e6bc3da2
Sarah Pratt <sarahmhpratt75@gmail.com>
2023-04-08 11:31:04 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Datacomp website
+Made of 3 main files:
+index.html (home page)
+leaderboard.html
+people.html
+workshop.html
+
+#running locally
+You have to create a local server to run locally because of the design of the leaderboard.
+I have hard-coded in the port number for now, so you have to do port 8000 or change it in all the 
+places it is mentioned in leaderboard.html.
+
+To run the website, navigate into the folder, running
+`python3 -m http.server 8000`
+
+and then go to http://localhost:8000/ in your browser
---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+const nav = document.querySelector(".header"),
+  hamBtn = document.querySelector(".ham__btn"),
+  navMenu = document.querySelector(".nav__menu"),
+  navLinks = document.querySelectorAll(".nav__link"),
+  sections = document.querySelectorAll("section[id]"),
+  themeBtn = document.querySelector(".theme__toggle");
+
+  // Hamburger menu start
+  hamBtn.addEventListener("click", (e) => {
+    navMenu.classList.toggle("show__menu");
+  });
+  
+  navLinks.forEach((e) => {
+    e.addEventListener("click", () => navMenu.classList.remove("show__menu"));
+  });
+  // Hamburger menu start
+
+
+// Current section view in nav links 
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 20,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navMenu
+        .querySelector("a[href*=" + sectionId + "]")
+        .classList.add("active__link");
+    } else {
+      navMenu
+        .querySelector("a[href*=" + sectionId + "]")
+        .classList.remove("active__link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+
+// Shadow add to header after scroll 
+const scrollHeader = () => {
+  if (window.scrollY >= 80) nav.classList.add("scroll__header");
+  else nav.classList.remove("scroll__header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+
+// Scroll to top button 
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
